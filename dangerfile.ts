7b0860b40fcd618a8e876207e771ecd697b4241d
pufflik <pufflik@gmail.com>
2020-10-22 00:04:36 +0300

    Update dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,5 +4,24 @@ const tsFiles = danger.git.fileMatch("**/*.ts")
 
 if(tsFiles.edited) {
   const wrongTsFiles = R.reject(R.anyPass([R.includes('types/'), R.includes('api/')]), tsFiles.getKeyedPaths().edited)
-  console.log(wrongTsFiles)
+
+  if(!R.isEmpty(wrongTsFiles)) {
+    fail(`
+These files should have \`.tsx\` extension:
+${wrongTsFiles.map(file => `\`${file}\` `)}
+    `)
+  }
+}
+
+const tsxFiles = danger.git.fileMatch("**/*.tsx")
+
+if(tsxFiles.edited) {
+  const wrongTsxFiles = R.filter(R.anyPass([R.includes('types/'), R.includes('api/')]), tsxFiles.getKeyedPaths().edited)
+
+  if(!R.isEmpty(wrongTsxFiles)) {
+    fail(`
+These files should have \`.ts\` extension:
+${wrongTsxFiles.map(file => `\`${file}\` `)}
+    `)
+  }
 }
